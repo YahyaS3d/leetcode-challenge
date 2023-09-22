4a42c197a8ff12d491f8ba81c1558ffb539e2646
Yahya <yahya412001@gmail.com>
2023-09-22 09:21:52 +0300

    Update Solution

---
+++ day 5/IsSubsequence.java
@@ -0,0 +1,14 @@
+class Solution { //check md file in day 5 folder! 
+    public boolean isSubsequence(String s, String t) {
+        int sPointer = 0, tPointer = 0;
+
+        while (sPointer < s.length() && tPointer < t.length()) {
+            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
+            }
+            tPointer++;
+        }
+
+        return sPointer == s.length();
+    }
+}
