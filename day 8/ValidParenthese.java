1f8d0215d665293e847e2ed4caa541c657dde811
Yahya <yahya412001@gmail.com>
2023-09-25 10:00:18 +0300

    memory-optimized solution

--- day 8/ValidParenthese.java
+++ day 8/ValidParenthese.java
@@ -1,29 +1,21 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (c == ')' && top != '(') {
-                    return false;
-                }
-                if (c == ']' && top != '[') {
-                    return false;
-                }
-                if (c == '}' && top != '{') {
-                    return false;
-                }
-            }
+        int n = s.length();
+        if (n % 2 != 0) {
+            return false; // If the string length is odd, it can't be valid.
         }
         
-        return stack.isEmpty();
+        char[] stack = new char[n];
+        int top = 0; // Index to keep track of the top of the stack.
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack[top++] = c;
+            } else if (top == 0 || (c == ')' && stack[--top] != '(') || (c == ']' && stack[--top] != '[') || (c == '}' && stack[--top] != '{')) {
+                return false;
+            }
+        }
+
+        return top == 0;
     }
 }
