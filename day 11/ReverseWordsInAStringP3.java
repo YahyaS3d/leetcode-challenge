8788c70a0d1e053b38354208b04a6e741fc0b57c
Yahya Saad (") <yahya412001@gmail.com>
2023-10-01 11:04:04 +0300

    Object Cloning solution

--- day 11/ReverseWordsInAStringP3.java
+++ day 11/ReverseWordsInAStringP3.java
@@ -1,36 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public String reverseWords(String s) {
         // Split the input string into words using space as the delimiter
         String[] words = s.split(" ");
         
-        // Initialize a StringBuilder to store the result
-        StringBuilder result = new StringBuilder();
+        // Initialize a List to store the result
+        List<String> result = new ArrayList<>();
         
         // Iterate through the words
         for (String word : words) {
-            // Reverse each word and append it to the result
+            // Reverse each word using object cloning and add it to the result
             String reversedWord = reverseWord(word);
-            result.append(reversedWord).append(" ");
+            result.add(reversedWord);
         }
         
-        // Remove the trailing space and return the result
-        return result.toString().trim();
+        // Join the reversed words with spaces and return the result
+        return String.join(" ", result);
     }
     
-    // Helper function to reverse a word
+    // Helper function to reverse a word using object cloning
     private String reverseWord(String word) {
-        char[] wordChars = word.toCharArray();
-        int left = 0;
-        int right = wordChars.length - 1;
+        StringBuilder reversed = new StringBuilder(word.length());
         
-        while (left < right) {
-            char temp = wordChars[left];
-            wordChars[left] = wordChars[right];
-            wordChars[right] = temp;
-            left++;
-            right--;
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed.append(word.charAt(i));
         }
         
-        return new String(wordChars);
+        return reversed.toString();
     }
 }
