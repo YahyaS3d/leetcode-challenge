f03a732165ba65a771fe4462eecb64bd9e7b983e
Yahya Saad (") <yahya412001@gmail.com>
2023-11-23 11:03:52 +0200

    adding corresponding digits

---
+++ day 17/list problems/TestAdd2Num.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example 1
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode result1 = solution.addTwoNumbers(l1, l2);
+        printList(result1);
+
+        // Example 2
+        ListNode l3 = new ListNode(0);
+        ListNode l4 = new ListNode(0);
+        ListNode result2 = solution.addTwoNumbers(l3, l4);
+        printList(result2);
+
+        // Example 3
+        ListNode l5 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        ListNode l6 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        ListNode result3 = solution.addTwoNumbers(l5, l6);
+        printList(result3);
+    }
+
+    private static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
